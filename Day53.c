d95fbd1a873e4206c562b523d14edbba2004d1f4
Rupesh  kumar <rk0095269@gmail.com>
2026-03-31 23:55:01 +0530

    Add files via upload

---
+++ Day53.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Queue node for BFS
+struct QNode {
+    struct Node* node;
+    int hd;
+};
+
+// Create new tree node
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = newNode(arr[0]);
+
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    int i = 1;
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Vertical Order Traversal
+void verticalOrder(struct Node* root) {
+    if (root == NULL) return;
+
+    // Queue for BFS
+    struct QNode queue[1000];
+    int front = 0, rear = 0;
+
+    // Map simulation (HD range)
+    int minHD = 0, maxHD = 0;
+
+    // Store nodes for each HD
+    int map[2000][100];  // [HD offset][values]
+    int size[2000] = {0};
+
+    // Push root
+    queue[rear++] = (struct QNode){root, 0};
+
+    while (front < rear) {
+        struct QNode temp = queue[front++];
+        struct Node* curr = temp.node;
+        int hd = temp.hd;
+
+        int index = hd + 1000; // shift to avoid negative index
+
+        map[index][size[index]++] = curr->data;
+
+        if (hd < minHD) minHD = hd;
+        if (hd > maxHD) maxHD = hd;
+
+        if (curr->left)
+            queue[rear++] = (struct QNode){curr->left, hd - 1};
+
+        if (curr->right)
+            queue[rear++] = (struct QNode){curr->right, hd + 1};
+    }
+
+    // Print vertical order
+    for (int i = minHD; i <= maxHD; i++) {
+        int index = i + 1000;
+        for (int j = 0; j < size[index]; j++) {
+            printf("%d ", map[index][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    verticalOrder(root);
+
+    return 0;
+}
