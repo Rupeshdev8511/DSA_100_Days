addf46cf239bd808e8c515e82759be2f132e98b1
Rupesh  kumar <rk0095269@gmail.com>
2026-04-04 16:42:40 +0530

    Add files via upload

---
+++ Day63.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+// Adjacency list node
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Graph structure
+struct Node* adj[MAX];
+int visited[MAX];
+
+// Create a new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge (undirected graph)
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+// DFS function
+void dfs(int v) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    struct Node* temp = adj[v];
+    while (temp != NULL) {
+        int neighbor = temp->vertex;
+        if (!visited[neighbor]) {
+            dfs(neighbor);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, edges, u, v, s;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    // Initialize adjacency list and visited array
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    printf("Enter number of edges: ");
+    scanf("%d", &edges);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &s);
+
+    printf("DFS Traversal: ");
+    dfs(s);
+
+    return 0;
+}
